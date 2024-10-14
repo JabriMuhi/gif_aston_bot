34eaaee6d403d77eca1281c95a7bb7d2d78bce30
JabriMuhi <jabri.muhi@gmail.com>
2024-10-14 17:38:35 +0300

    feat: added sample jdbc connection

--- src/main/java/org/jabrimuhi/Main.java
+++ src/main/java/org/jabrimuhi/Main.java
@@ -5,9 +5,33 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.sql.*;
+
 public class Main {
+    private static final String URL = "jdbc:postgresql://localhost:5432/aston_gif_bot";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "a6d3fdd4";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(new GifBot());
+
+        try {
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM example");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("first_name"));
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
